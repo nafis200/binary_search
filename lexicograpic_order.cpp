26c5d52343edfcdb160e5ae0176ca01514255497
nafis200 <nafisahamed14@gmail.com>
2025-07-04 14:57:25 +0600

    done

--- lexicograpic_order.cpp
+++ lexicograpic_order.cpp
@@ -1,3 +1,37 @@
+// wrong
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     string s, t; cin >> s >> t;
+//     int a = s.size();
+//     int b = t.size();
+//     int mn = min(a, b);
+//     bool ok = false;
+//     for(int i = 0; i < mn;){
+//         if(s[i] < t[i]){
+//             ok = true;
+//             break;
+//         }
+//         else if(s[i] == t[i]){
+//             i++;
+//         }
+//         else{
+//             break;
+//         }
+//     }
+//     if(ok && a < b){
+//         cout << "Yes\n";
+//     }
+//     else{
+//        cout << "No\n";
+//     }
+// }
+
+//  s = "abc" t = "at"
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -14,6 +48,7 @@ int main(){
             cout << "No\n";
             return 0;
         }
+        // else continue loop
     }
     if(a < b) cout << "Yes\n";
     else cout << "No\n";
